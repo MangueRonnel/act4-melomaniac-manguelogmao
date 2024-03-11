5e3204b16ef5090c0ad75f606c9220a06c1c42af
MangueRonnel <ronnelmangue720@gmail.com>
2024-03-11 19:10:22 +0800

    song five

---
+++ song5.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <title>Song 5</title>
+<link rel="stylesheet"href="style.css">
+</head>
+<body>
+
+<?php
+echo"<div class='box'>";
+
+echo"<pre>";
+echo"\nAgainst All Odds";
+echo"\n[Intro]";
+ 
+echo"\nDm7  D7sus2  G     x2";
+ 
+ 
+echo"\n[Verse 1]";
+ 
+echo"\nAm7                    Bm7";
+echo"\nHow can I just let you walk away";
+echo"\nC               Dm7";
+echo"\nJust let you leave without a trace,";
+echo"\nF                 G/F               Em7  Am";
+echo"\nWhen I stand here taking every breath with you, ooh.";
+echo"\nDm           F                 Gsus4 G";
+echo"\nYou're the only one who really knew me at all.";
+echo"\nAm7                    ";
+echo"\nHow can you just walk away from me,";
+echo"\nC                     Dm7";
+echo"\nWhen all I can do is watch you leave,";
+echo"\nF                   G";
+echo"\nCause we've shared the laughter and the pain,";
+echo"\nEm7             Am";
+echo"\nAnd even shared the tears.";
+echo"\nDm           F                 Gsus4 G";
+echo"\nYou're the only one who really knew me at all.";
+ 
+ 
+echo"\n[Chorus]";
+echo"\nC/G";
+echo"\nSo take a look at me now";
+echo"\nD/G";
+echo"\nCause there's just an empty space,";
+echo"\nAm                   F";
+echo"\nAnd there's nothing left here to remind me,";
+echo"\nDm             Gsus4";
+echo"\nJust the memory of your face.";
+echo"\nG          C/G";
+echo"\nOh take a look at me now";
+echo"\nD/G";
+echo"\nCause there's just an empty space,";
+echo"\nAm             F";
+echo"\nAnd you coming back to me is against the odds,";
+echo"\nDm               Gsus4  G";
+echo"\nAnd that's what I've got to face.";
+ 
+ 
+echo"\n[Verse 2]";
+echo"\nAm7                         Bm7";
+echo"\nI wish I could just make you turn around,";
+echo"\nC                      Dm7";
+echo"\nTurn around and see me cry,";
+echo"\nF              G/F";
+echo"\nThere's so much I need to say to you,";
+echo"\nEm7          Am";
+echo"\nSo many reasons why.";
+echo"\nDm           F                 Gsus4       G";
+echo"\nYou're the only one who really knew me at all.";
+ 
+ 
+echo"\n[Chorus]";
+echo"\nC/G";
+echo"\nSo take a look at me now";
+echo"\nD/G";
+echo"\nCause there's just an empty space,";
+echo"\nAm                     F";
+echo"\nAnd there's nothing left here to remind me,";
+echo"\nDm             Gsus4";
+echo"\nJust the memory of your face.";
+echo"\nG          C/G";
+echo"\nOh take a look at me now";
+echo"\nD/G";
+echo"\nCause there's just an empty space,";
+echo"\nAm                        F";
+echo"\nBut to wait for you, well that's all I can do,";
+echo"\nDm               Gsus4";
+echo"\nAnd that's what I've got to face.";
+echo"\nG          C/G";
+echo"\nTake a good look at me now";
+echo"\nD/G";
+echo"\nCause I'll still be standing here,";
+echo"\nAm             F";
+echo"\nAnd you coming back to me is against all odds,";
+echo"\nDm                 Gsus4       G";
+echo"\nIt's the chance I've got to take";
+ 
+ 
+echo"\n[Outro]";
+echo"\nDm7  D7sus2  G     x2";
+echo"\nDm7  C  G  F  G";
+echo"\nTake a look at me now.";
+
+
+?>
+echo"</div>";
+
+</body>
+</html>
