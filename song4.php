c300a1d650ac82f3d3d2ef3dbe4f47bb608cba77
MangueRonnel <ronnelmangue720@gmail.com>
2024-03-11 19:06:38 +0800

    index

---
+++ song4.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <title>Song 4</title>
+<link rel="stylesheet"href="style.css">
+</head>
+<body>
+
+<?php
+echo"<div class='box'>";
+
+echo"<pre>";
+echo"\nSomewhere Only we Know";
+echo"\n[Intro]";
+echo"\nC   C/B   Dm   Gsus4   G   x2";
+ 
+echo"\n[Verse 1]";
+echo"\nC              C/B";
+echo"\nI walked across an empty land";
+echo"\nDm                          Gsus4          G";
+echo"\nI knew the pathway like the back of my hand";
+echo"\nC               C/B";
+echo"\nI felt the earth beneath my feet";
+echo"\nDm                       Gsus4        G";
+echo"\nSat by the river, and it made me complete";
+ 
+
+echo"\n[Pre-Chorus]";
+echo"\nAm               Em";
+echo"\nOh, simple thing, where have you gone?";
+echo"\nDm                           F             G";
+echo"\nI'm getting old and I need something to rely on";
+echo"\nAm             Em";
+echo"\nSo tell me when you're gonna let me in";
+echo"\nDm                             F             G";
+echo"\nI'm getting tired and I need somewhere to begin";
+ 
+ 
+echo"\n[Verse 2]";
+echo"\nC              C/B";
+echo"\nI came across a fallen tree";
+echo"\nDm                         Gsus4     G";
+echo"\nI felt the branches of it looking at me";
+echo"\nC                C/B";
+echo"\nIs this the place we used to love?";
+echo"\nDm                               Gsus4    G";
+echo"\nIs this the place that I've been dreaming of?";
+ 
+ 
+echo"\n[Pre-Chorus]";
+echo"\nAm               Em";
+echo"\nOh, simple thing, where have you gone?";
+echo"\nDm                          F              G";
+echo"\nI'm getting old and I need something to rely on";
+echo"\nAm             Em";
+echo"\nSo tell me when you're gonna let me in";
+echo"\nDm                             F             G";
+echo"\nI'm getting tired and I need somewhere to begin";
+ 
+ 
+echo"\n[Chorus]";
+echo"\nF                 C                 G";
+echo"\nAnd if you have a minute, why don't we go";
+echo"\nF             C              G";
+echo"\nTalk about it somewhere only we know?";
+echo"\nF                 C           G";
+echo"\nThis could be the end of everything";
+echo"\nDm";
+echo"\nSo why don't we go";
+echo"\nEm                     Dm";
+echo"\nSomewhere only we know?";
+echo"\nEm                   Dm    G";
+echo"\nSomewhere only we know?";
+ 
+ 
+echo"\n[Pre-Chorus]";
+echo"\nAm                Em";
+echo"\nOh, simple thing, where have you gone?";
+echo"\nDm                         F              G";
+echo"\nI'm getting old and I need something to rely on";
+echo"\nAm              Em";
+echo"\nSo tell me when you're gonna let me in";
+echo"\nDm                           F              G";
+echo"\nI'm getting tired and I need somewhere to begin";
+ 
+ 
+echo"\n[Chorus]";
+echo"\nDm                C/E               G";
+echo"\nAnd if you have a minute, why don't we go";
+echo"\nDm                C/E         G";
+echo"\nTalk about it somewhere only we know?";
+echo"\nDm                C/E          G";
+echo"\nThis could be the end of everything";
+echo"\nDm";
+echo"\nSo why don't we go?";
+echo"\nEm";
+echo"\nSo why don't we go?";
+ 
+echo"\nOoh, aah over Instrumental";
+echo"\nDm    C  G  x2";
+ 
+ 
+echo"\n[Outro]";
+echo"\nDm                C           G";
+echo"\nThis could be the end of everything";
+echo"\nDm";
+echo"\nSo why don't we go";
+echo"\nEm             C       Dm";
+echo"\nSomewhere only we know?";
+echo"\n(Slowly)";
+echo"\nEm                  Dm";
+echo"\nSomewhere only we know";
+echo"\nEm             Dm    C";
+echo"\nSomewhere only we know";
+
+
+echo"</div>";
+
+?>
+
+</body>
+</html>
